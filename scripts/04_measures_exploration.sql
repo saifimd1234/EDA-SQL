3dc32bc75382c0192404c0f6b15f7b0529f34af0
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-17 01:02:50 +0530

    Generate a Report that shows all key metrics of the business.

--- scripts/04_measures_exploration.sql
+++ scripts/04_measures_exploration.sql
@@ -33,3 +33,15 @@ SELECT COUNT(customer_key) AS total_customers FROM gold.dim_customers;
 -- Find the total number of customers that has placed an order
 SELECT COUNT(DISTINCT customer_key) AS total_customers FROM gold.fact_sales;
 
+-- Generate a Report that shows all key metrics of the business
+SELECT 'Total Sales' AS measure_name, SUM(sales_amount) AS measure_value FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Quantity', SUM(quantity) FROM gold.fact_sales
+UNION ALL
+SELECT 'Average Price', AVG(price) FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Orders', COUNT(DISTINCT order_number) FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Products', COUNT(DISTINCT product_name) FROM gold.dim_products
+UNION ALL
+SELECT 'Total Customers', COUNT(customer_key) FROM gold.dim_customers;
